202218848baad197c2c4493b24a97b96d52ccd29
parents <parents@Jeremie-Win8-1>
2017-03-04 09:19:12 -0500

    Added square

--- HelloGuys/src/com/dufrois/herve/Hello.java
+++ HelloGuys/src/com/dufrois/herve/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Bonjour!");
+		float f = 5.4F;
+		System.out.println("Square: " + Compute.square(f));
 	}
 
 }
