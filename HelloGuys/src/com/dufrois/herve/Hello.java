9164b8111cbe74735791629d5636a3b94c908c99
parents <parents@Jeremie-Win8-1>
2017-05-23 21:43:01 -0400

    Added text

--- HelloGuys/src/com/dufrois/herve/Hello.java
+++ HelloGuys/src/com/dufrois/herve/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Bonjour!");
 		float f = 5.4F;
-		System.out.println("Square of " + f + "is: " + Compute.square(f));
+		System.out.println("The square of " + f + "is: " + Compute.square(f));
 	}
 
 }
