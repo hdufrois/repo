139899fb85eaf3d06010d53450d858a33b07dc43
hdufrois <hdufrois@yahoo.com>
2017-07-22 17:35:53 -0400

    Added call to private method

--- HelloGuys/src/com/dufrois/herve/Compute.java
+++ HelloGuys/src/com/dufrois/herve/Compute.java
@@ -2,6 +2,11 @@ package com.dufrois.herve;
 
 public class Compute {
 	public static float square(float f){
+		System.out.println("public method called!");
 		return f*f;
+	}
+	private static int twice (int i) {
+		System.out.println("private method called!");
+		return 2*i;
 	}
 }
--- HelloGuys/src/com/dufrois/herve/Hello.java
+++ HelloGuys/src/com/dufrois/herve/Hello.java
@@ -1,11 +1,28 @@
 package com.dufrois.herve;
 
+import java.lang.reflect.Method;
+
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Bonjour!");
+		System.out.println("Calling a public method:");
 		float f = 5.4F;
-		System.out.println("The square of " + f + "is: " + Compute.square(f));
+		System.out.println("The square of " + f + " is: " + Compute.square(f));
+		System.out.println("\nCalling a private method:");
+		Compute computer = new Compute();
+		Class<? extends Compute> clazz = computer.getClass();
+		for (Method method : clazz.getDeclaredMethods()) {
+			    if ("twice".equals(method.getName())) {
+			    	method.setAccessible(true);
+			    	try {
+			    		int i = 2;
+	 			    	Integer result = (Integer)method.invoke(computer, i);
+			    		System.out.println("2 times " + i + " is " + result);
+			    	} catch (Exception ex) {
+			    		System.out.println("Exception " + ex);
+			    	}
+			    }
+			}
 	}
 
 }
